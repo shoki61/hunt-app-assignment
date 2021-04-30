a09c527f0f0f6cc3548b91001fe7cdcee64548ac
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-04-30 21:40:27 +0300

    create main header component

---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import SImage from 'react-native-scalable-image';
+
+import styles from './style'
+
+const MainHeader = () => (
+    <View>
+        <Text>Main Header</Text>
+    </View>
+);
+
+export default MainHeader;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => (
     <View style={styles.navBar}>
         <View style={styles.navBarLeft}>
             <TouchableOpacity style={styles.backBtn}><SImage height={17} source={images.arrow}/></TouchableOpacity>
-            <Text style={styles.navTitle}>League of Legend</Text>
+            <Text style={styles.navTitle}>League of Legends</Text>
         </View>
         <TouchableOpacity><SImage width={20} source={images.menu}/></TouchableOpacity>
     </View>
