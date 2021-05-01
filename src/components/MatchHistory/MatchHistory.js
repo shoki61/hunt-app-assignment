1129ff036192330609d3d609863c34b99b50d7ff
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-05-01 22:28:26 +0300

    set tab view in MatchHistory

--- src/components/MatchHistory/MatchHistory.js
+++ src/components/MatchHistory/MatchHistory.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import SImage from 'react-native-scalable-image';
 
+import SandTimer from '../../assets/images/sand_timer.svg';
+import ChampionInverted from '../../assets/images/champion_inverted.svg';
+import RunesInverted from '../../assets/images/runes_inverted.svg';
+import ScoreInverted from '../../assets/images/score_inverted.svg';
+import ItemsInverted from '../../assets/images/items_inverted.svg';
+import MinionInverted from '../../assets/images/minion_inverted.svg';
+import PositionInverted from '../../assets/images/position_inverted.svg';
+import Group_506 from '../../assets/images/Group_506.svg';
+
+
 import styles from './style';
 
 const MatchHistory = () => (
-    <View>
-        <Text>Match History</Text>
+    <View style={styles.matchHistory}>
+        <View style={styles.tab}>
+            <SandTimer width={15} height={15}/>
+            <PositionInverted width={15} height={15}/>
+            <ChampionInverted width={15} height={15}/>
+            <RunesInverted width={15} height={15}/>
+            <ScoreInverted width={15} height={15}/>
+            <ItemsInverted width={15} height={15}/>
+            <MinionInverted width={15} height={15}/>
+            <Group_506 width={15} height={15}/>
+        </View>
+        <View>
+            
+        </View>
     </View>
 );
 
--- src/components/MatchHistory/style.js
+++ src/components/MatchHistory/style.js
@@ -1,7 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    matchHistory:{
+        width: '100%',
+        paddingHorizontal: 15,
+        marginVertical:5
+    },
+    tab: {
+        width:'100%',
+        height:30,
+        backgroundColor:'#303030',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        paddingHorizontal:25
+    }
 });
 
 export default styles;
