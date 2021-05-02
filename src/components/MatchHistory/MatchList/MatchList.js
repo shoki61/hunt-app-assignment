d68edebe150454fe7f32aca0bcb535840d364cf9
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-05-02 23:11:42 +0300

    create MatchList component

---
+++ src/components/MatchHistory/MatchList/MatchList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './style';
+
+const MatchList = () => (
+    <View> </View>
+);
+
+export default MatchList;
